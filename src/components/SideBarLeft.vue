a5183b6ea5f76662c7fd7213da91c90eed0a1dec
MiPinzon <mipinzong@unal.edu.co>
2024-04-03 20:44:26 -0500

    Sidebar y perfil antes de la validaciones

---
+++ src/components/SideBarLeft.vue
@@ -0,0 +1,127 @@
+<template> 
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <meta charset=”utf-8”>
+        <link rel="icon" type="image/png" href="../assets/LogoThotex.png">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        
+        <ul class="sidebar">
+            <li class="logo-container">
+                <header>
+                    <img src="../assets/LogoThotex.png" alt="Logo de la empresa">
+                </header>
+            </li>
+
+            <li class="tablero">
+                <RouterLink to="/tablero" class="table">
+                    <label>Tablero</label>
+                    <ion-icon name="clipboard-outline"></ion-icon>
+                </RouterLink>
+                
+            </li>
+           
+            <li>
+                <label>Nominas</label>
+                <ion-icon name="id-card-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Ventas</label>
+                <ion-icon name="pricetag-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Compras</label>
+                <ion-icon name="cart-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Inventario</label>
+                <ion-icon name="reader-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Terceros</label>
+                <ion-icon name="person-add-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Contabilidad</label>
+                <ion-icon name="calculator-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Calendario</label>
+                <ion-icon name="calendar-outline"></ion-icon>
+            </li>
+
+            <li>
+                <label>Perfil</label>
+                <ion-icon name="person-outline"></ion-icon>
+            </li>
+        </ul>
+    </body>
+
+
+</template>
+
+<style>
+
+    .sidebar{
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: #203B70;
+        height: 100%;
+        display: flex;
+        flex-direction: column;  
+        padding-left: 0;   
+        box-sizing: border-box;  
+        margin-top: 0;
+    }
+            
+    li{
+                
+        width: 85px;
+        height: 70px;
+        background-color: #203B70;
+        font-size: 12px;
+        font-weight: bold;
+                color: white;
+                box-sizing: border-box;
+                display: flex;
+                flex-direction: column;
+                text-align: center;
+                align-items: normal;
+            }
+            li:hover{
+                background-color: #4D6FB3;
+            }
+            
+
+            img{
+                width: 80px;
+                align-items: center;
+            }
+
+    ion-icon{
+        margin-top: 3px;
+        font-size: 45px;
+    }
+
+    .table{
+        text-decoration: none;
+        color: white;
+        
+    } 
+    
+    .logo-container:hover{
+        background-color: #203B70;
+
+    }
+    
+
+</style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,234 @@
+<template> 
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <meta charset=”utf-8”>
+        <link rel="icon" type="image/png" href="../assets/LogoThotex.png">
+        <meta name="viewport" content="width=device-width">
+        
+        
+
+    </head>
+
+    <body>
+        <div>
+            <SideBarLeft></SideBarLeft>
+        </div>
+        <main>
+            <header>
+                <h1>Perfil</h1>
+            </header>
+            <form method="post" action="/" class="formulario">
+
+                <section class="encabezado">
+                    <h2>Información personal</h2>
+                    <h2>Información de contacto</h2>
+                </section>
+               
+                
+                <section class="etiqueta1">
+                    <label class="descripcion" >Rol</label>
+                    <label class="descripcion" for="email">Email<span>*</span></label>
+
+                </section>
+
+                <section class="campo1">
+                    
+                    <select name="why" class="rol">
+                        <option value="1">Independiente</option>
+                        <option value="2">Empresa</option>  
+                    </select>
+                    
+                    <input class="campo-entrada" type="email" id="email" name="email" required>
+
+                </section>  
+
+                <section class="etiqueta2">
+                    <label class="descripcion" >Tipo de identificación</label>
+                    <label class="descripcion" for="telefono">Teléfono</label>
+                </section>
+
+                <section class="campo2">
+                    <div class="div-identificacion">
+                        <section class="">
+                            <select class="selec-identificacion">
+                            <option value="1">Número identificación tributar</option>
+                            <option value="2">xxxxxxxxx</option>  
+                            </select>
+                        </section>
+                        <label class="descripcion" for="iden">Numero de indetificación<span>*</span></label>
+                        <input class="campo-entrada" type="number" id="iden" name="iden" required>
+                    </div>
+
+                    <div>
+                    
+                    <input class="campo-entrada" type="tel" id="telefono" name="telefono" >
+                    </div>
+
+                </section>
+
+
+                <div class="contenedor">
+
+                    
+                    
+                    
+                    <div>
+                        <label class="descripcion" for="social">Razón social / Nombre completo</label>
+                        <input class="campo-entrada" type="text" id="social" name="social">
+                    </div>
+                    
+                    <div>
+                        <label class="descripcion" for="ubi">Municipio / Departamento</label>
+                        <input class="campo-entrada" type="text" id="ubi" name="ubi">
+                    </div>
+                
+
+                <div>
+                    <button type="submit" id="guardar">Guardar información</button>
+                </div>
+            </div>
+            
+            </form>
+                
+
+
+        </main>
+
+    </body>
+    
+</template>
+
+<script setup lang="ts">
+    import SideBarLeft from '../components/SideBarLeft.vue';
+</script>
+
+<style>
+body {
+    background-color:rgb(211, 208, 203) ;
+    font-family: "roboto";
+    position: flex;
+}
+
+button {
+    position: relative;
+    background: #FCBB01;
+    border: 0;
+    border-radius: 13px;
+    height: 35px;
+    font-family: "roboto";
+    font-weight: bold;
+    margin-top: 200px;
+    margin-bottom: 15px;
+    left: 10px;
+}
+
+button:hover{
+    background: #ffe6a1;
+}
+
+.formulario{
+    position: absolute;
+    border: 0; 
+    background-color: white;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    top: 10px;
+    height: 85%;
+    width: 90%;
+    margin-left: 95px;
+    margin-top: 65px;
+    text-align: justify;
+    
+}
+.campo-entrada{
+    position: relative;
+    left: 10px;
+    display: flex;
+    flex-direction: column; 
+    border: 0 ;
+    height: 20px;
+    box-sizing: border-box;
+    border-bottom: 2px solid black;                
+    margin-bottom: 20px;
+    width: 200px;
+}
+
+h2{
+    margin-left: 10px;
+    font-weight: bold;
+    color: black;
+    
+}
+.descripcion{
+    color: #203B70;
+    font-weight: bold;
+    position: relative;
+    left: 10px;
+}
+.div-rol{
+    margin-left: 10px;
+    margin-bottom: 10px;
+}
+
+.rol{
+    width: 200px;
+    height: 30px;
+    border-radius: 3px;
+    margin-left: 10px;
+}
+.rol:hover{
+    background-color: #E6E3E5;
+    
+    }
+
+.selec-identificacion{
+    margin-left: 10px;
+    margin-bottom: 10px;
+    width: 200px;
+    height: 30px;
+}
+
+.selec-identificacion:hover{
+    background-color: #E6E3E5;
+}
+
+.encabezado{
+    display: flex;
+    justify-content: start;
+    gap: 200px;
+}
+
+.etiqueta1{
+    display: flex;
+    justify-content: start;
+    gap: 420px;
+}
+
+.campo1{
+    display: flex;
+    justify-content: start;
+    gap: 235px;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+  }
+  
+  input[type=number]::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  .etiqueta2{
+    display: flex;
+    justify-content: start;
+    gap: 285px;
+  }
+
+  .campo2{
+    display: flex;
+    justify-content: start;
+    gap: 232px;
+  }
+</style>
