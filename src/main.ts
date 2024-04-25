09528cba7ec1af8397181c89cd923e25406b8023
Juan David Vásquez Pinzón <juandavidvasquezpinzon@gmail.com>
2024-04-25 18:42:30 -0500

    removido vuex

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 
 import { Icon } from '@iconify/vue'
 
 const app = createApp(App)
 app.component('IconifyIcon', Icon)
 
-app.use(store).use(router).mount('#app')
+app.use(router).mount('#app')
