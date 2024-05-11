a20ab7e1a1aaf3e25526a60d7396925b63af0908
MiPinzon <mipinzong@unal.edu.co>
2024-05-11 18:26:02 -0500

    Cards de inventory view

--- src/views/PortalViews/InventoryView.vue
+++ src/views/PortalViews/InventoryView.vue
@@ -3,9 +3,9 @@
 <body class="cuerpo">
     <h1 class="title">Inventario</h1>
     <div class="cartas">
-        <CardComponent v-bind:class="'card-global'" v-bind:image="imagenCarta" v-bind:title="'Registrar productos'" v-bind:content="'Puedes registrar productos'"></CardComponent>
-        <CardComponent v-bind:class="'card-global'" image="@/assets/logoamarillo.png" v-bind:title="'Historial de inventario'" v-bind:content="'Puedes registrar productos'"></CardComponent>
-        <CardComponent v-bind:class="'card-global'" image="@/assets/logoamarillo.png" v-bind:title="'Estadisticas de inventario'" v-bind:content="'Puedes registrar productos'"></CardComponent>
+        <CardComponent v-bind:class="'card-global'" image="https://i.imgur.com/aLkaA1D.png" v-bind:title="'Registrar productos'" v-bind:content="'Puedes registrar productos'"></CardComponent>
+        <CardComponent v-bind:class="'card-global'" image="https://i.imgur.com/ICFBcKG.png" v-bind:title="'Historial de inventario'" v-bind:content="'Puedes registrar productos'"></CardComponent>
+        <CardComponent v-bind:class="'card-global'" image="https://i.imgur.com/yo86YnL.png" v-bind:title="'Estadísticas de inventario'" v-bind:content="'Puedes registrar productos'"></CardComponent>
     </div>
 
     </body>
@@ -15,7 +15,6 @@
 
 <script setup lang="ts">
     import CardComponent from '../../components/PortalComponents/CardComponent.vue';
-    var imagenCarta = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRffHlYOp7ivHArSLTABCkOTtoKDIJxh2w345KLiRb8Bw&s';
 
 </script>
 
