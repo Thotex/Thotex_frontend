08cf99d68a2418b90b6be854e9b57d06d8aa22f2
MiPinzon <mipinzong@unal.edu.co>
2024-05-15 17:28:01 -0500

    footer

--- src/views/HomePageViews/PricesView.vue
+++ src/views/HomePageViews/PricesView.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-        <h1>Prices</h1>
+    <div class="contenedor">
+        <h1>Precios</h1>
+        <section>
+            <CardComponent v-bind:class="'card-global'" image="https://i.imgur.com/LwOBLeM.png" v-bind:title="'Juan'" v-bind:content="'asdad'"></CardComponent>
+            <CardComponent v-bind:class="'card-global'" v-bind:title="'14900'" v-bind:content="''"></CardComponent>
+
+        </section>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import CardComponent from '@/components/PortalComponents/CardComponent.vue';
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .contenedor{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    section{
+        display: flex;
+    }
 </style>
