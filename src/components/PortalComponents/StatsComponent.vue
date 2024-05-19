20535d495edc4b2ed487db37aff87e846688a2c9
MiPinzon <mipinzong@unal.edu.co>
2024-05-19 13:44:43 -0500

    Gráfico de pie para el mes

--- src/components/PortalComponents/StatsComponent.vue
+++ src/components/PortalComponents/StatsComponent.vue
@@ -1,36 +1,102 @@
 <template>
-    <div class="card-global">
-        <canvas id="lineChart" height="100"></canvas>
-    </div>
+    <div>
+    <canvas ref="pieChart"></canvas>
+  </div>
 </template>
 
 <script setup lang="ts">
-    import Chart from 'chart.js/auto';
-    import { onMounted } from 'vue';
+    import { useSalesStore } from '@/stores/sales';
+    import { onMounted, ref } from 'vue';
+    import { Chart, registerables } from 'chart.js'
+    import { ISale } from '@/interfaces/ISales';
 
-    onMounted(() => {
-        const canvas = document.querySelector('#lineChart') as HTMLCanvasElement;
-        if (!canvas) return;
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                {
-                    label: 'Dataset 1',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    borderColor: 'rgb(75, 192, 192)',
-                    tension: 0.1
-                }
-                ]
+    Chart.register(...registerables)// Registrar todos los componentes de Chart.js
+    const pieChart = ref(null);
+    const salesStore = useSalesStore();
+
+    interface RGBColor {
+        r: number
+        g: number
+        b: number
+    }
+
+    const getRandomColor = (): RGBColor => {
+        const r = Math.floor(Math.random() * 255)
+        const g = Math.floor(Math.random() * 255)
+        const b = Math.floor(Math.random() * 255)
+        return { r, g, b }
+    }
+
+    const rgbaColor = ({ r, g, b }: RGBColor, alpha: number): string => `rgba(${r}, ${g}, ${b}, ${alpha})`
+
+    const darkenColor = ({ r, g, b }: RGBColor, percentage: number): RGBColor => {
+        const factor = 1 - percentage
+        return {
+        r: Math.floor(r * factor),
+        g: Math.floor(g * factor),
+        b: Math.floor(b * factor)
+        }
+    }
+    // Función para agrupar ventas por mes y calcular el total
+    const groupSalesByMonth = (sales: ISale[]) => {
+    const monthlySales: { [key: string]: number } = {}
+    sales.forEach(sale => {
+        const month = new Date(sale.Fac_fechaGeneracion).toLocaleString('default', { month: 'long' })
+            if (!monthlySales[month]) {
+                monthlySales[month] = 0
             }
-        });
-    });
+        monthlySales[month] += sale.Fac_precioTotal
+    })
+    return monthlySales
+}
+
+const createChart = () => {
+  if (pieChart.value) {
+    const monthlySales = groupSalesByMonth(salesStore.dataList)
+    const labels = Object.keys(monthlySales)
+    const data = Object.values(monthlySales)
+    const backgroundColors: string[] = []
+    const borderColors: string[] = []
+
+    labels.forEach(() => {
+      const baseColor = getRandomColor()
+      backgroundColors.push(rgbaColor(baseColor, 0.2))
+      borderColors.push(rgbaColor(darkenColor(baseColor, 0.1), 1))
+    })
+
+    new Chart(pieChart.value, {
+      type: 'pie',
+      data: {
+        labels, // Etiquetas de los meses
+        datasets: [{
+          label: 'Ventas',
+          data, // Datos del gráfico
+          backgroundColor: backgroundColors, // Colores aleatorios para el fondo
+          borderColor: borderColors, // Colores oscuros para el borde
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top'
+          }
+        }
+      }
+    })
+  }
+}
+
+onMounted(async () => {
+  await salesStore.fetchDataList() // Obtén los datos de ventas
+  createChart() // Crea el gráfico
+})
 </script>
 
 <style scoped lang="scss">
-    
+    canvas {
+        max-width: 600px;
+        max-height: 400px;
+    }
 </style>
