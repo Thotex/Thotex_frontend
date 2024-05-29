443da8148dfd0c41a733427af9111e27af443203
Juan David Vásquez Pinzón <juandavidvasquezpinzon@gmail.com>
2024-05-29 16:26:51 -0500

    Cambios temporales

--- src/views/HomePageViews/PricesView.vue
+++ src/views/HomePageViews/PricesView.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="contenedor">
         <h1>Precios</h1>
+        <MercadoPagoComponent />
+
         <section>
             <CardComponent v-bind:class="'card-global'" image="https://i.imgur.com/LwOBLeM.png" v-bind:title="'Juan'" v-bind:content="'asdad'"></CardComponent>
             <CardComponent v-bind:class="'card-global'" v-bind:title="'14900'" v-bind:content="''"></CardComponent>
 
         </section>
+
     </div>
 </template>
 
 <script setup lang="ts">
 import CardComponent from '@/components/PortalComponents/CardComponent.vue';
+import MercadoPagoComponent from '@/components/MercadoPagoComponent.vue';
 </script>
 
 <style lang="scss" scoped>
