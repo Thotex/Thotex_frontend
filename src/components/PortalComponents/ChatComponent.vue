29438c350a3c63e1ac04d0eff9ecb9182f8da0c9
Juan David <juandavidvasquezpinzon@gmail.com>
2024-08-15 10:33:54 -0500

    bases para chatbot

---
+++ src/components/PortalComponents/ChatComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai';
+
+    const genAI = new GoogleGenerativeAI(process.env.VUE_APP_GOOGLE_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+    const generationConfig = {
+        temperature: 0.5,
+        topP: 0.95,
+        topK: 64,
+        maxOutputTokens: 8192,
+        responseMimeType: "text/plain",
+    };
+
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        },
+        /*
+        {
+            category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        }
+        */
+    ]
+
+    async function run() {
+    const chatSession = model.startChat({
+        generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+        history: [
+        ],
+    });
+
+    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+    console.log(result.response.text());
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
